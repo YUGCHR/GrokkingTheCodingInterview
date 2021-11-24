d534db3ec9d3a1ed21bd7207c4de154dbf88e9a2
Yuriy <yuri.gonchar@gmail.com>
2021-11-24 12:29:25 +0200

    Add project files.

---
+++ PatternSlidingWindow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PatternSlidingWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grokking the Coding Interview");
+            Console.WriteLine("Pattern: Sliding Window");
+            Console.WriteLine("Smallest Subarray with a given sum");
+
+        }
+    }
+}
