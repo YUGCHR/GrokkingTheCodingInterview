d8808b3e37b30e0f1b2d977ee1d59cc7f24b8e9d
Yuriy <yuri.gonchar@gmail.com>
2021-11-26 15:07:44 +0200

    the solution with dictionary way worked

--- PatternSlidingWindow/LongestSubstringWithMaximumKDistinctCharacters.cs
+++ PatternSlidingWindow/LongestSubstringWithMaximumKDistinctCharacters.cs
@@ -12,7 +12,10 @@ namespace PatternSlidingWindow
         {
             Console.WriteLine("Class - Longest substring with maximum K distinct characters");
 
-            string input = "aaarrraaarrraaaci";
+            string input = "caaarrraaarrraaaciiiiiiiiiiiiiiiiiiirrr";
+            // caaarrraaa r r r a a a c i i i i iiiiiiiiiiiiiiirrr;
+            // 01234567891011121314151617181920;
+            int k = 3;
             string blank0 = "  ";
 
             for (int i = 0; i < input.Length; i++)
@@ -29,8 +32,7 @@ namespace PatternSlidingWindow
                 Console.Write($"{blank0}{i},");
             }
             Console.WriteLine("\n-------------------------------------------------------------------------------------------------------------------------");
-
-            int k = 2;
+                        
             Console.WriteLine($"String input is {input}");
             Console.WriteLine($"Necessary K Distinct Characters is {k}");
 
@@ -42,86 +44,96 @@ namespace PatternSlidingWindow
         public static int FindLongestSubstring(string input, int k)
         {
             // завести словарь, где ключ - символ, значение - индекс
+            Dictionary<char, int> countDistinctCharacters = new();
+
             // можно индекс назвать rightFrameSide, а можно просто i
-            // в цикле по строке достать символ, проверить, есть ли в словаре
-            // если есть, записать значение нового индекса
-            // записать этот индекс в переменную leftFrameSide
-            // если нет, посмотреть на размер словаря (хранить в отдельной переменной?)
-            // можно размер хранить в bool - isListFull
-            // если влазит, добавить в словарь
-            // если полный, найти минимальный индекс (как?), его ключ удалить
-            // c# find key with min value in dictionary
-            // https://stackoverflow.com/questions/23734686/c-sharp-dictionary-get-the-key-of-the-min-value
-            // https://stackoverflow.com/questions/2805703/good-way-to-get-the-key-of-the-highest-value-of-a-dictionary-in-c-sharp
-            // var min = tempDictionary.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
-            // похоже, здесь l - это TAccumulate, а r - TSource (наоборот разницы нет?)
-            // хотя должно быть наоборот - https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.aggregate?view=net-6.0
-            // никакой магии, просто тупо перебрать словарь
-            // похоже, этот индекс будет как раз в leftFrameSide
-            // измерить размер окна - текущий индекс минус leftFrameSide
-            // сравнить его с максимальным foundLongestSubstring
-            // если больше, сохранить в него, иначе забыть
-            // опять найти минимальный индекс в словаре и записать его в leftFrameSide
-            // словарь не полный, добавить в него новый символ 
-            // ехать дальше
-
-
-
+            int leftFrameSide = 0;
             int foundLongestSubstring = 0;
-            int currentSubstringLength = 0;
-            int countDistinctCharacters = 0;
-            int index = -1;
-            char[] kDistinctCharacters = new char[k];
+            bool isListFull = false; // Dictionary is emplty (or not full)
 
-            // insert in kDistinctCharacters first K distinct chars
-            int i = 0;
-            bool kDistinctCharactersIsFull = false;
-            bool continueWhile = true;
-
-            while (continueWhile)
+            for (int i = 0; i < input.Length; i++)
             {
-                index++;
-                char indexChar = input[index];
+                char indexChar = input[i];
+                Console.WriteLine($"- {i} - loop of cycle, indexChar is {indexChar}, leftFrameSide = {leftFrameSide}, the longest substring length = {foundLongestSubstring}, dictionary is full = {isListFull}");
 
-                Console.WriteLine($"while step in string input {index} - i = {i}, k = {k}, indexChar = {indexChar}");
-
-                bool charIsMissingInChain = true;
-                for (int j = 0; j < k; j++)
+                // в цикле по строке достать символ, проверить, есть ли в словаре
+                if (countDistinctCharacters.ContainsKey(indexChar))
                 {
-                    if (indexChar == kDistinctCharacters[j])
-                    {
-                        charIsMissingInChain = false;
-                    }
+                    // если есть, записать значение нового индекса
+                    Console.WriteLine($"Yes, artist IS at home today, for char {indexChar} new index {i} will be set in dictionary");
+
+                    countDistinctCharacters[indexChar] = i;
                 }
-
-                if (charIsMissingInChain)
+                // если такого символа в словаре нет, надо его добавить, а если нет места, сначала удалить самый ранний символ
+                else
                 {
-                    if (!kDistinctCharactersIsFull)
+                    // если нет, посмотреть на размер словаря (хранить в отдельной переменной?)
+                    // можно размер хранить в bool - isListFull
+                    Console.WriteLine($"New char {indexChar} with index {i} will be added in dictionary, which is full {isListFull}");
+
+                    if (isListFull)
                     {
-                        kDistinctCharacters[i] = indexChar;
-                        Console.WriteLine($"charIsMissingInChain is {charIsMissingInChain} - kDistinctCharacters[{i}] = {indexChar}");
-                        i++;
-                        if (i >= k)
+                        // если полный, найти минимальный индекс (как?), его ключ удалить
+                        // c# find key with min value in dictionary
+                        // https://stackoverflow.com/questions/23734686/c-sharp-dictionary-get-the-key-of-the-min-value
+                        // https://stackoverflow.com/questions/2805703/good-way-to-get-the-key-of-the-highest-value-of-a-dictionary-in-c-sharp
+                        // похоже, здесь l - это TAccumulate, а r - TSource (наоборот разницы нет?)
+                        // хотя должно быть наоборот - https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.aggregate?view=net-6.0
+                        // никакой магии, просто тупо перебрать словарь
+                        // похоже, этот индекс будет как раз в leftFrameSide
+                        // нет, надо искать именно минимальный индекс (Value) в словаре (как получить сразу оба значения за один проход?)
+                        char minChar = countDistinctCharacters.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+                        int minValue = countDistinctCharacters[minChar];
+                        Console.WriteLine($"dictionary is full {isListFull}, the farest char {minChar} was found with index {minValue} and will be removed from");
+
+                        // определить, сравнить и, если надо, зафиксировать размер цепочки
+                        // измерить размер окна - текущий индекс минус leftFrameSide
+                        int thisChainLength = i - leftFrameSide;
+                        Console.WriteLine($"new substring length {thisChainLength} was defined and will be compared with max found length {foundLongestSubstring}");
+
+                        // сравнить его с максимальным foundLongestSubstring
+                        if (thisChainLength > foundLongestSubstring)
                         {
-                            kDistinctCharactersIsFull = true;
+                            // если больше, сохранить в него, иначе забыть
+                            foundLongestSubstring = thisChainLength;
+                            Console.WriteLine($"new max found length {foundLongestSubstring} was set");
+
+                        }
+
+                        // удаляем кандидата на выбывание - самый последний встреченный символ
+                        countDistinctCharacters.Remove(minChar);
+                        //isListFull = false;
+                        Console.WriteLine($"dictionary is full {isListFull}, the farest char {minChar} was found with index {minValue} and will be removed from");
+
+                        // опять найти минимальный индекс в словаре и записать его в leftFrameSide
+                        // записать новый индекс в переменную leftFrameSide - (minValue + 1) будет новым началом окна
+                        leftFrameSide = minValue + 1;
+
+                        // словарь не полный, добавить в него новый символ 
+                    }
+                    //else // - else убрать, словарь всегда будет неполный, даже если был полный
+                    // если влазит, добавить в словарь
+                    countDistinctCharacters.Add(indexChar, i);
+                    Console.WriteLine($"dictionary is full = {isListFull}, new char {indexChar} with index {i} was added, new dictionary length is {countDistinctCharacters.Count}");
+
+                    if (!isListFull)
+                    {
+                        int countDistinctCharactersLenght = countDistinctCharacters.Count;
+                        if (countDistinctCharactersLenght == k)
+                        {
+                            isListFull = true;
+                            Console.WriteLine($"dictionary became full = {isListFull}, its length is {countDistinctCharacters.Count}");
+
                         }
                     }
-                    else
-                    {
-                        continueWhile = false;
-                    }
                 }
+                // ехать дальше по строке
             }
 
-            string output = String.Join("", kDistinctCharacters);
-            Console.WriteLine($"kDistinctCharacters is {output}, index in input string stay on {index} position");
+            string output = String.Join("", countDistinctCharacters.Keys);
+            Console.WriteLine($"kDistinctCharacters is {output}, the longest substring length = {foundLongestSubstring}");
 
-
-
-
-
-
-            return 0;
+            return foundLongestSubstring;
         }
     }
 }
