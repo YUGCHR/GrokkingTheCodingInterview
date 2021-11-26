99f45a40197d51c2cbf43f6fd821f367c4236a42
Yuriy <yuri.gonchar@gmail.com>
2021-11-26 11:27:24 +0200

    this is looks like plan with Dictionary

--- PatternSlidingWindow/LongestSubstringWithMaximumKDistinctCharacters.cs
+++ PatternSlidingWindow/LongestSubstringWithMaximumKDistinctCharacters.cs
@@ -41,6 +41,32 @@ namespace PatternSlidingWindow
 
         public static int FindLongestSubstring(string input, int k)
         {
+            // завести словарь, где ключ - символ, значение - индекс
+            // можно индекс назвать rightFrameSide, а можно просто i
+            // в цикле по строке достать символ, проверить, есть ли в словаре
+            // если есть, записать значение нового индекса
+            // записать этот индекс в переменную leftFrameSide
+            // если нет, посмотреть на размер словаря (хранить в отдельной переменной?)
+            // можно размер хранить в bool - isListFull
+            // если влазит, добавить в словарь
+            // если полный, найти минимальный индекс (как?), его ключ удалить
+            // c# find key with min value in dictionary
+            // https://stackoverflow.com/questions/23734686/c-sharp-dictionary-get-the-key-of-the-min-value
+            // https://stackoverflow.com/questions/2805703/good-way-to-get-the-key-of-the-highest-value-of-a-dictionary-in-c-sharp
+            // var min = tempDictionary.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+            // похоже, здесь l - это TAccumulate, а r - TSource (наоборот разницы нет?)
+            // хотя должно быть наоборот - https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.aggregate?view=net-6.0
+            // никакой магии, просто тупо перебрать словарь
+            // похоже, этот индекс будет как раз в leftFrameSide
+            // измерить размер окна - текущий индекс минус leftFrameSide
+            // сравнить его с максимальным foundLongestSubstring
+            // если больше, сохранить в него, иначе забыть
+            // опять найти минимальный индекс в словаре и записать его в leftFrameSide
+            // словарь не полный, добавить в него новый символ 
+            // ехать дальше
+
+
+
             int foundLongestSubstring = 0;
             int currentSubstringLength = 0;
             int countDistinctCharacters = 0;
@@ -89,6 +115,11 @@ namespace PatternSlidingWindow
 
             string output = String.Join("", kDistinctCharacters);
             Console.WriteLine($"kDistinctCharacters is {output}, index in input string stay on {index} position");
+
+
+
+
+
 
             return 0;
         }
