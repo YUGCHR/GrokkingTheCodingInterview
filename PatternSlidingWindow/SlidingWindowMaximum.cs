74bf56bae9ac8892469c9065f1ed5b330b7c47f0
Yuriy <yuri.gonchar@gmail.com>
2021-12-07 15:06:37 +0200

    SlidingWindowMaximum started to work

--- PatternSlidingWindow/SlidingWindowMaximum.cs
+++ PatternSlidingWindow/SlidingWindowMaximum.cs
@@ -12,8 +12,14 @@ namespace PatternSlidingWindow
         {
             Console.WriteLine("Class - Sliding Window Maximum");
 
-            int[] input = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+            //int[] input = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+            // output 3, 3, 5, 5, 6, 7
+            int[] input = new int[] { 1, 9, 3, 1, 1, 1, 1, 1, 1, -1, -3, -3, -3, 9, 9, 9, 9, 9, -5, 3, -6, 7 };
             int k = 3;
+            // output 9, 9, 3, 1, 1, 1, 1, 1, 1, -1, -3, 9, 9, 9, 9, 9, 9, 9, 3, 7
+            //int k = 5;
+            // output 9, 9, 3, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9
+
 
             for (int i = 0; i < input.Length; i++)
             {
@@ -54,36 +60,99 @@ namespace PatternSlidingWindow
             int stepCounter = 0;
             int inputLength = input.Length;
             int outputLength = inputLength - k + 1; //наверное
+            int leftFrameSide = 0;
 
             int[] output = new int[outputLength];
+            int outIndex = 0;
 
             List<int> sortedIndices = new();
-            sortedIndices.Add(input[0]); // 1
+            //sortedIndices.Add(0);
+                                         
+            // first K steps has completed
+            bool firstKStepsHasCompleted = false;
 
-            for (int i = 1; i < k; i++)
+            // предположим, что теперь это основной цикл
+            // тогда после первых К символов, надо начинать вычитать символы из хвоста
+            // точнее, как только окно вырастет до нужного размера, оно должно начать двигаться
+            // для этого можно измерять длину окна в конце каждого цикла и, если размер (на 1) больше, чем надо, двигать левую сторону
+            // (второй) while для этого не нужен, поскольку всегда двигаем на 1 символ
+            // и потом не забыть выделить результат (максимум) текущего окна
+
+            for (int i = 0; i < inputLength; i++)
             {
                 bool isWritten = false;
                 int n = 0;
+                Console.WriteLine($"\n--- CYCLE loop {i} is started, isWritten = {isWritten}, n = {n}, leftFrameSide = {leftFrameSide}");
 
-                while(!isWritten && n < sortedIndices.Count)
+                while (!isWritten && n < sortedIndices.Count)
                 {
-                    if (input[i] > sortedIndices[n])
+                    // проверяем все числа списка и если нашлось меньшее (точнее, если новое оказалось больше), вставляем новое перед найденным
+                    // test all the numbers in the list and if found a smaller one (more precisely, if the new one turned out to be larger), insert a new one before the found one
+                    int element = input[sortedIndices[n]];
+                    if (input[i] > element)
                     {
-                        sortedIndices.Insert(n, input[i]);
+                        sortedIndices.Insert(n, i);
+                        // запись произошла, устанавливаем признак и добавляем индекс из-за удлинившегося списка
+                        // the write in has occurred, we set the tag isWritten and add the index due to the lengthened list
                         isWritten = true;
                         n++;
-                        Console.WriteLine($"IF input[{i}] = {input[i]} > sortedIndices[{n}] = {sortedIndices[n]}, isWritten = {isWritten}");
+                        Console.WriteLine($"IF input[{i}] = {input[i]} > sortedIndices[{n}] = {sortedIndices[n]}, isWritten = {isWritten}, n={n}");
 
                     }
+                    // will examine the next list element if exist
                     Console.WriteLine($"Cycle n {n} - sortedIndices.Count = {sortedIndices.Count}");
                     n++;
                 }
-
                 if (!isWritten)
                 {
-                    sortedIndices.Add(input[i]);
+                    // список закончился, а меньшего елемента так и не нашлось, поэтому добавляем новое число в конец списка
+                    // the list is over, but no smaller element was found, so we add a new number to the end of the list
+                    sortedIndices.Add(i);
 
                 }
+                
+                // calculate the length of the window at the end of each cycle
+                int windowSize = i - leftFrameSide;
+                Console.WriteLine($"calculate the length of the window - windowSize={windowSize} = i={i} - leftFrameSide={leftFrameSide} + 1");
+
+                string sortedIndicesRearyToString = String.Join(", ", sortedIndices);
+                Console.WriteLine($"LIST is ready - Cycle {i} - sortedIndices length is {sortedIndices.Count} = {sortedIndicesRearyToString} and k={k}");
+
+                // if the window size has reached the specified K
+                // it makes sense only for the first K steps, so then this condition can be disabled
+                if (firstKStepsHasCompleted || windowSize == k-1)
+                {
+                    // take the maximum from the current position of the sliding window - this is the zero item in the list
+                    output[outIndex] = input[sortedIndices[0]];
+
+                    // once we got here, the window has already formed and it is no longer need to check its size
+                    firstKStepsHasCompleted = true;
+
+                    // move output index for the next result
+                    outIndex++;
+
+                    // remove from the list the index of the item to be removed 
+                    // since the list is already in descending order, removing any element will not break its ordering (hopefully)
+                    // нет! int index = sortedIndices[leftFrameSide]; ключевой момент - надо найти в списке элемент со значением leftFrameSide
+                    // find an element in the list with the value leftFrameSide
+
+                    string sortedIndicesRemovedToString = String.Join(", ", sortedIndices);                    
+                    Console.WriteLine($"WILL try to remove = {leftFrameSide} from = {sortedIndicesRemovedToString} with length = {sortedIndices.Count} and k = {k}");
+
+                    bool wasRemoved = sortedIndices.Remove(leftFrameSide);
+
+                    sortedIndicesRemovedToString = String.Join(", ", sortedIndices);
+                    Console.WriteLine($"WAS REMOVED = {wasRemoved} - Cycle {i} - sortedIndices element was removed and new length is {sortedIndices.Count} = {sortedIndicesRemovedToString} and k={k}");
+
+                    // move the left side forward
+                    leftFrameSide++;
+                }
+
+
+
+
+
+
                 string sortedIndicesToString = String.Join(", ", sortedIndices);
                 Console.WriteLine($"Cycle {i} - sortedIndices with length {k} = {sortedIndicesToString}");
             }
@@ -94,9 +163,9 @@ namespace PatternSlidingWindow
             //string sortedIndicesToString = String.Join(", ", sortedIndices);
             //Console.WriteLine($"sortedIndices with length {k} = {sortedIndicesToString}");
 
-            
 
-            Console.WriteLine($"+++++++++++ SOLUTION IS --> maximums of sliding windows with size {k} are {output}, input string length = {inputLength}, steps count = {stepCounter}");
+            string outputToString = String.Join(", ", output);
+            Console.WriteLine($"+++++++++++ SOLUTION IS --> maximums of sliding windows with size {k} are {outputToString}, input string length = {inputLength}, steps count = {stepCounter}");
 
             return output;
         }
---
+++ PatternSlidingWindowTests/SlidingWindowMaximumTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatternSlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSlidingWindow.Tests
+{
+    [TestClass()]
+    public class SlidingWindowMaximumTests
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 1, 9, 3, 1, 1, 1, 1, 1, 1, -1, -3, -3, -3, 9, 9, 9, 9, 9, -5, 3, -6, 7 }, 3, new int[] { 9, 9, 3, 1, 1, 1, 1, 1, 1, -1, -3, 9, 9, 9, 9, 9, 9, 9, 3, 7 })]
+        [DataRow(new int[] { 1, 9, 3, 1, 1, 1, 1, 1, 1, -1, -3, -3, -3, 9, 9, 9, 9, 9, -5, 3, -6, 7 }, 5, new int[] { 9, 9, 3, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9 })]
+        [DataRow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3, new int[] { 3, 3, 5, 5, 6, 7 })]
+        [DataRow(new int[] { 9, 7, 1, 5, 2, 8, 6, 0 }, 3, new int[] { 9, 7, 5, 8, 8, 8 })]
+        [DataRow(new int[] { 1 }, 1, new int[] { 1 })]
+        [DataRow(new int[] { 1, -1 }, 1, new int[] { 1, -1 })]
+        [DataRow(new int[] { 9, 11 }, 2, new int[] { 11 })]
+        [DataRow(new int[] { 4, -2 }, 2, new int[] { 4 })]
+
+        public void FindSlidingWindowMaximumTest(int[] input, int k, int[] expectedOutput)
+        {
+            int[] foundOutput = SlidingWindowMaximum.FindSlidingWindowMaximum(input, k);
+
+            CollectionAssert.AreEqual(expectedOutput, foundOutput);
+        }
+    }
+}
