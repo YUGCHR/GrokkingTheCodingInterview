97864a9f5aa330a02b6f7850507e77ec4bf3b31f
Yuriy <yuri.gonchar@gmail.com>
2021-12-15 12:09:18 +0200

    FindDistinctEchoSubstringsTest was added

---
+++ PatternSlidingWindowTests/DistinctEchoSubstringsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatternSlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSlidingWindow.Tests
+{
+    [TestClass()]
+    public class DistinctEchoSubstringsTests
+    {
+        [TestMethod()]
+        [DataRow("abcabcabc", 3)]
+        [DataRow("leetcodeleetcode", 2)]
+        [DataRow("abcabcabcleetcodeleetcode", 5)]
+        [DataRow("aba", 0)]
+        [DataRow("aa", 1)]
+
+        public void FindDistinctEchoSubstringsTest(string input, int expectedOutput)
+        {
+            int result = DistinctEchoSubstrings.FindDistinctEchoSubstrings(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
